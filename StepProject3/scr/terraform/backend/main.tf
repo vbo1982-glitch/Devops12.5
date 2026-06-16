365845b480b257e44f3f10fee58abfee3984867a
Vitaliy Bogoslavskiy <vbo1982@gmail.com>
2026-06-16 16:17:42 +0300

    feat: add main infrastructure terraform code (vpc, ec2, sg)

---
+++ StepProject3/scr/terraform/backend/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
---
+++ StepProject3/scr/terraform/backend/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  type    = string
+  default = "my-step-project-3-tf-state-bucket-123" # Обов'язково змініть значення на унікальне
+}
